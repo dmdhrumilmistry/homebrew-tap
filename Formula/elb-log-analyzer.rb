04f52db19c11ad51928631b8812de685876a2ffb
dmdhrumilmistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2023-07-22 14:01:46 +0530

    fix typo

--- Formula/elb-log-analyzer.rb
+++ Formula/elb-log-analyzer.rb
@@ -8,7 +8,7 @@ class ElbLogAnalyzer < Formula
 
   depends_on "python3"
 
-  rresource "elb-log-analyzer" do
+  resource "elb-log-analyzer" do
     url "https://files.pythonhosted.org/packages/f7/87/9992f3e566175b1e7089852780539426f8369973ad017e3403b1457c3769/elb_log_analyzer-0.4.1.tar.gz"
     sha256 "166030aef792e439b82bcbcc0c8b57ccd37107f051e110ed1ae304bc82304ab6"
   end
